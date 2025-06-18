62dfff03934a4c6f8271538deeabf9af32b241ec
João Pedro <joaopdadv@gmail.com>
2025-06-18 13:15:39 -0300

    estrutura de rotas inicial sveltekit

---
+++ front/src/routes/home/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import "../../app.css";
+
+    let { children } = $props();
+</script>
+
+<h1>Minha Navbar</h1>
+
+{@render children()}
---
+++ front/src/routes/home/+page.svelte
@@ -0,0 +1,3 @@
+<h2>HOME</h2>
+<a href="/home/login">login button</a>
+<a href="/home/register">register button</a>
